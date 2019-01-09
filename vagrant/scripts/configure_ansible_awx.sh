1b105d12f2a383bf715aefc9ff3081ea81af89b7
Adam Rush <adamrushuk@gmail.com>
2019-01-09 08:11:25 +0000

    Added Ansible Tower CLI configuration script

---
+++ vagrant/scripts/configure_ansible_awx.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Vars
+awx_host_url="http://192.168.10.20"
+awx_username="admin"
+awx_password="password"
+awx_projects_source_folder="/vagrant/ansible-projects/"
+awx_projects_dest_folder="/var/lib/awx/projects"
+azure_credential_file_path="/vagrant/vagrant/scripts/azure_ansible_credentials.yml"
+ssh_public_key_path="$HOME/.ssh/id_rsa.pub"
+
+# Create SSH key
+if [ ! -f "$ssh_public_key_path" ]
+then
+    echo -e "\nINFO: Started Creating new SSH key..."
+    echo -e "\n\n\n" |  ssh-keygen -t rsa -C "dev@adamrushuk.github.io" -N ""
+else
+    echo -e "\nINFO: SSH key already exists...SKIPPING."
+fi
+ssh_public_key=`cat "$ssh_public_key_path"`
+
+# Configure Ansible AWX using Tower CLI
+echo -e "\nINFO: Started Configuring Ansible AWX using Tower CLI..."
+
+# Configure host - include "http:" as it default to HTTPS
+tower-cli config host $awx_host_url
+
+# Disable SSL verification to allow insecure HTTP traffic
+tower-cli config verify_ssl false
+
+# Configure login
+tower-cli config username $awx_username
+tower-cli config password $awx_password
+
+# Copy projects folder
+if [ -d "$awx_projects_source_folder" ]
+then
+    echo -e "\nINFO: Copying Ansible Projects folder(s) for AWX..."
+    rsync -avz "$awx_projects_source_folder"* $awx_projects_dest_folder
+else
+    echo -e "\nINFO: Ansible Projects source folder missing...SKIPPING."
+fi
+
+# Create project
+echo -e "\nINFO: Creating Azure Project in AWX..."
+tower-cli project create --name "Azure Project" --description "Azure Playbooks" --scm-type "manual" --local-path "azure-linux-vm" --organization "Default"
+
+# Create Azure inventory
+# TODO: add var for public ssh"$HOME/.ssh/id_rsa.pub"
+echo -e "\nINFO: Creating Azure Inventory in AWX..."
+
+tower-cli inventory create --name "Azure Inventory" --description "Azure Inventory" --organization "Default" --variables "ssh_public_key: \"$ssh_public_key\""
+
+# Create Azure credential
+echo -e "\nINFO: Creating Azure Credential in AWX..."
+tower-cli credential create --name "Azure Credential" --description "Azure Credential" --organization "Default" --credential-type "Microsoft Azure Resource Manager" --inputs "@$azure_credential_file_path"
+
+# Create Azure job template for a simple Resource Group
+echo -e "\nINFO: Creating job template for a simple Azure Resource Group..."
+# WORKAROUND: you must supply an SSH credential type initially
+tower-cli job_template create --name "Azure Resource Group" --description "Azure Resource Group - Job Template" --inventory "Azure Inventory" --project "Azure Project" --playbook "resource_group.yml" --credential "Demo Credential"
+# WORKAROUND: you can then associate an Azure credential afterwards
+tower-cli job_template associate_credential --job-template "Azure Resource Group" --credential "Azure Credential"
+
+# Create Azure job template for a CentOS Linux VM and all required resources
+echo -e "\nINFO: Creating job template for a CentOS Linux VM and all required resources in Azure..."
+# WORKAROUND: you must supply an SSH credential type initially
+tower-cli job_template create --name "Azure CentOS Linux VM" --description "Azure CentOS Linux VM - Job Template" --inventory "Azure Inventory" --project "Azure Project" --playbook "centos_vm.yml" --credential "Demo Credential"
+# WORKAROUND: you can then associate an Azure credential afterwards
+tower-cli job_template associate_credential --job-template "Azure CentOS Linux VM" --credential "Azure Credential"
